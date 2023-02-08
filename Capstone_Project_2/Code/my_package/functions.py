edbb378d692b3a609d77da2b8823f2191f2ce0a7
Hiram <hiram.g.menendez@gmail.com>
2023-02-08 12:46:31 +0800

    Added Final Report

--- Capstone_Project_2/Code/my_package/functions.py
+++ Capstone_Project_2/Code/my_package/functions.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def make_image_table(dataframe, figsize, figheight=0.1, frame=False, axis='off'):
+def make_image_table(dataframe, figsize=None, frame=False, axis='off', cellLoc='center', fontsize=None):
     fig, ax = plt.subplots(figsize=figsize)
-    fig.set_figheight(figheight)
     ax.set_frame_on(frame)
     ax.axis(axis)
-    pd.plotting.table(ax, dataframe)
+    pd.plotting.table(ax, dataframe, fontsize=fontsize, cellLoc=cellLoc)
     plt.show()
